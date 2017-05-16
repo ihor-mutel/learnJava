5a84c14e914d2dbbbc780aed686ffad19f739a9f
web1991t <The Revenant@TheRevenant-PC.riversoft.net>
2017-05-16 12:47:01 +0300

    named Student (it extends a  Person, it has an access to all of the Person classes)

---
+++ src/firstproject/Student.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firstproject;
+
+/**
+ *
+ * @author The Revenant
+ */
+public class Student extends Person{
+    
+    private int grade; //percentage
+    
+    public Student(String first, String last) {
+        super(first, last);
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+    
+}
